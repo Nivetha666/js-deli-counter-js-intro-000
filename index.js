3cc0e41fe9440409cf95d71e98f2def8de6595c9
Nivetha <nivedha.orton@gmail.com>
2019-08-14 16:42:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function nowServing(kartzdeliline){
   for(let i=0 ; i < kartzdeliline.length ; i++){
 
   }
-  if (katzDeli.length === 0) {
+  if (kartzdeliline.length === 0) {
     return "There is nobody waiting to be served!";
   }
   else
-  return (`Currently serving ${katzDeli.shift()}.`);
+  return (`Currently serving ${kartzdeliline.shift()}.`);
 }
